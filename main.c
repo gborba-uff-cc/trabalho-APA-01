0b3e885c95e30a87ea725004084f1a59c2cfb46b
Gabriel Borba <gborba@id.uff.br>
2021-07-22 21:13:32 -0300

    declarações das funções para o heapsort

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>;
 #include <stdlib.h>;
 
+// heapsort.h ------------------------------------------------------------------
+void heapsort(int *array, int n);
+void heapify(int *array, int n);
+void deleteMax(int *array, int n);
+void bubbleDown(int *array, int i, int n);
+void swap(int *array, int i, int j);
+
+// heapsort.c ------------------------------------------------------------------
 int main(int argc, char const *argv[]) {
     printf("Ola mundo");
     return 0;
