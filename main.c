13ce1848d18e96039ad546d5f47ee56e1811e137
Gabriel Borba <gborba@id.uff.br>
2021-07-22 21:21:39 -0300

    escrita da função swap

--- main.c
+++ main.c
@@ -9,6 +9,16 @@ void bubbleDown(int *array, int i, int n);
 void swap(int *array, int i, int j);
 
 // heapsort.c ------------------------------------------------------------------
+//* troca o elemento na posição i com o elemento na posição j */
+void swap(int *array, int i, int j) {
+    int temp;
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    return;
+}
+
+// -----------------------------------------------------------------------------
 int main(int argc, char const *argv[]) {
     printf("Ola mundo");
     return 0;
