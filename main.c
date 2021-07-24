79fc5e8262f702684a326e9d38025b69dc2126fc
Gabriel Borba <gborba@id.uff.br>
2021-07-24 10:50:38 -0300

    função ordenacaoMetodoCaixas foi escrita

--- main.c
+++ main.c
@@ -86,6 +86,53 @@ void swap(int *array, int i, int j)
     return;
 }
 
+// ordenacaoMetodoCaixas.h -----------------------------------------------------
+/** Ordena, em ordem ordem crescente, um array de inteiros com n elementos.
+ *
+ * obs.: Os elementos devem pertecer ao intervalo [a,b]
+ *
+ * obs.: A ordenação é feita usando o método das caixas (método das caixas é um 
+ * caso particular do bucket sort);
+ */
+void ordenacaoMetodoCaixas();
+
+// ordenacaoMetodoCaixas.h -----------------------------------------------------
+void ordenacaoMetodoCaixas(int *array, int n, int minValue, int maxValue)
+{
+    if (array == NULL) {
+        return;
+    }
+    int nCaixas = maxValue -minValue +1;
+    int caixas[nCaixas];
+    int i = 0;
+    int j = 0;
+
+    // para cada caixa
+    for (i = 0; i < nCaixas; i++) {
+        // inicializa a posição com 0
+        caixas[i] = 0;
+    }
+
+    // contabiliza o total de elementos de cada valor que existe no array
+    for (i = 0; i < n; i++) {
+        // aumenta a contagem na caixa correspondente ao elemento no array[i]
+        caixas[array[i] -minValue] = caixas[array[i] -minValue] +1;
+    }
+
+    // para cada caixa
+    for (i = 0, j = 0; i < nCaixas; i++) {
+        // enquanto a caixa não estiver vazia
+        while (caixas[i] != 0) {
+            // coloca o elemento na posição correta
+            array[j] = j +minValue;
+            // diminui a contagem da caixa
+            caixas[i] = caixas[i] -1;
+            // avança uma posição no array
+            j = j +1;
+        }
+    }
+}
+
 // util.h ----------------------------------------------------------------------
 /** Testa se o heapsort está ordenando em ordem crescente */
 bool testHeapsort();
